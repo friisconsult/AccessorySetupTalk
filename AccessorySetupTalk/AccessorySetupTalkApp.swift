95b440396e5e5f4455f7327dab4ec01cc551a508
Per Friis <per.friis@friisconsult.com>
2024-11-05 09:33:26 +0100

    Initial Commit

---
+++ AccessorySetupTalk/AccessorySetupTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessorySetupTalkApp.swift
+//  AccessorySetupTalk
+//
+//  Created by Per Friis on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct AccessorySetupTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessorySetupTalkTests/AccessorySetupTalkTests.swift
@@ -0,0 +1,17 @@
+//
+//  AccessorySetupTalkTests.swift
+//  AccessorySetupTalkTests
+//
+//  Created by Per Friis on 05/11/2024.
+//
+
+import Testing
+@testable import AccessorySetupTalk
+
+struct AccessorySetupTalkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
